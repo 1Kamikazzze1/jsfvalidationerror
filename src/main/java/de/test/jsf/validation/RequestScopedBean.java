e90094869ecfc2a720bb36ac0809cdc6aa58486e
1Kamikazzze1 <34446362+1Kamikazzze1@users.noreply.github.com>
2017-12-11 16:00:49 +0100

    Update RequestScopedBean.java

--- src/main/java/de/test/jsf/validation/RequestScopedBean.java
+++ src/main/java/de/test/jsf/validation/RequestScopedBean.java
@@ -1,12 +1,3 @@
-/**
- * Copyright (c) 2017-2017, Freistaat Bayern
- *
- * This software is unless otherwise specified property of the State of Bavaria.
- *
- * Unless required by applicable law or agreed to in writing, this software is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied.
- */
 package de.test.jsf.validation;
 
 import javax.enterprise.context.RequestScoped;
